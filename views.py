51b1b9fcdacbd59930515c2f18caeb0c760235b4
Honza-m <jan.morawiec@outlook.com>
2018-03-02 23:18:42 +0000

    Initial commit

--- views.py
+++ views.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+#imports
+import os, psycopg2
+
 @app.route('/')
 def index():
-    return "All works well"
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        con = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        r = cur.execute("""SELECT * FROM Users""")
+        return str(r)
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
