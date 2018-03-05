6afdbe59d1a6a0211b281b8425390fab15755364
Honza-m <jan.morawiec@outlook.com>
2018-03-05 00:22:32 +0000

    Flask debug commit

--- news/__init__.py
+++ news/__init__.py
@@ -7,10 +7,13 @@ logger.addHandler(ch)
 
 import os
 
+
 from flask import Flask
 app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "".join(random.choice(string.ascii_letters) for _ in range(10))
 app.config['SECURITY_PASSWORD_SALT'] = "MWfYKxtEqBEaBY0gIntk9TFMc"
+from news import views
